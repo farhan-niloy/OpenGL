ab2292102d3d1de2cbeefc263e60652867662441
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-04-24 11:54:27 +0600

    add GLEW & GLFW

--- Main/Main/main.cpp
+++ Main/Main/main.cpp
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+
+int main() {
+    
+    
+    return 0;
+}
--- OpenGL Coding/Main/main.cpp
+++ OpenGL Coding/Main/main.cpp
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+// Window Dimensions
+const GLint WIDTH = 800, HEIGHT = 600;
+
+
+int main() {
+    
+    // Initialise GLFW
+    if(glfwInit())
+    {
+        printf("GLFW Initialisation Failed!");
+        glfwTerminate();
+        return 1;
+    }
+    
+    // Setup GLFW window properties
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    
+    // Core profile = No backwards compatiblity
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    
+    
+    return 0;
+}
